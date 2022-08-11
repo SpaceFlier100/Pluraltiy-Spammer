a38d19eb25a662ca752229c29a91ce4f5deb3a1a
SpaceFlier100 <108110429+SpaceFlier100@users.noreply.github.com>
2022-08-11 10:50:19 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import time
-botint = 2700
-delay = 10
+botint = 0
+timeout = 10
 options = Options()
 options.add_argument("--window-size=1920,1080");
 options.add_argument("--start-maximized");
@@ -26,17 +26,17 @@ while True:
     password = driver.find_element(By.NAME, "password")
     verify = driver.find_element(By.NAME, "verify")
     button = driver.find_element(By.XPATH, "/html/body/div/main/section[1]/article[1]/form/button")
-    email.send_keys("spamemail" + botstr + "@gmail.com")
+    email.send_keys("notabottt" + botstr + "@gmail.com")
     print(botstr)
     password.send_keys("12345678")
     verify.send_keys("12345678")
     button.click()
     buttonpath = "/html/body/div[1]/div[2]/main/section[4]/div/div[2]/a[1]/button"
-    WebDriverWait(driver, delay).until(ec.presence_of_element_located((By.XPATH, buttonpath)))
+    WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, buttonpath)))
     respond = driver.find_element(By.XPATH, buttonpath)
     respond.click()
-    WebDriverWait(driver, delay).until(ec.presence_of_element_located((By.NAME, "vote")))
-    WebDriverWait(driver, delay).until(ec.presence_of_element_located((By.NAME, "_action")))
+    WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.NAME, "vote")))
+    WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.NAME, "_action")))
     ans = driver.find_element(By.NAME, "vote")
     enterans = driver.find_element(By.NAME, "_action")
     ans.send_keys("yourmum")
